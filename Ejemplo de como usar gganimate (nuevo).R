adb0121f72d0191fd6ddf9f1a3ac743b11b2f5ca
Amadeo Guzman <40497655+AmadeoGC@users.noreply.github.com>
2019-01-02 16:29:56 -0300

    Add files via upload

---
+++ Ejemplo de como usar gganimate (nuevo).R
@@ -0,0 +1,70 @@
+##********************************************************
+##  ---- Ejemplo de como usar el nuevo gganamite()   -----
+##********************************************************
+
+
+#Link ejemplo: https://robjhyndman.com/hyndsight/ftsviz/
+#Link gganimate/github: https://github.com/thomasp85/gganimate
+
+
+#_____________________
+### 1.- Librerias ----
+#_____________________
+library(tidyverse)
+library(demography)
+
+#install.packages(c("transformr", "devtools"))
+#devtools::install_github("thomasp85/gganimate")
+library(gganimate)
+
+
+#________________________________________________
+### 2.- Base de datos (libreria demography)  ----
+#________________________________________________
+
+frmort <- set.upperage(fr.mort, 100)
+# Create tibble
+frmort <- tibble(
+  year = rep(frmort$year, rep(length(frmort$age), length(frmort$year))),
+  age = rep(frmort$age, length(frmort$year)),
+  female = c(frmort$rate$female),
+  male = c(frmort$rate$male)) %>%
+  gather(male, female, key = "sex", value = "mortrate")
+
+head(frmort)
+
+#__________________________
+## 3.- Gráfico en ggplot2  ----
+#__________________________
+
+#Gráfico oriinal
+frmort %>%
+  ggplot(aes(x = age, y = mortrate, group = year, col = year)) +
+  geom_line() +
+  facet_grid(~sex) +
+  scale_y_log10() +
+  xlab("Age") + ylab("Log mortality") +
+  scale_color_gradientn(colours = rainbow(10))
+
+#_______________________________
+## 4.- ggplot2 + gganimate  ----
+#_______________________________
+
+#se debe guardar en un objeto en R, para despues crear el archivo en el pc
+(p <- frmort %>%
+  filter(year > 1900) %>%
+  ggplot(aes(x = age, y = mortrate, group = year, col = year)) +
+  geom_line() +
+  xlab("Age") + ylab("Log mortality") +
+  facet_grid(~sex) +
+  scale_y_log10() +
+  scale_color_gradientn(colours = rainbow(10)) +
+  #gganimet code
+  transition_time(year) +
+  ease_aes('linear') +
+  shadow_mark(colour = "grey70") +
+  labs(title = 'Year: {frame_time}')
+)
+
+#guardar GIF
+anim_save("C:/Users/aguzman/Desktop/ejemplo.gif", p,  width = 1500, height = 1500)
